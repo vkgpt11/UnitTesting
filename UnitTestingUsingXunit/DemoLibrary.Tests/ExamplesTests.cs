94256c4d67442283f1de77b4e53e6ca52f4a413c
vikas-kr-gupta <vikasgupta.net@live.com>
2019-01-13 21:29:59 +0530

    - Unit Testing via XUnit

---
+++ UnitTestingUsingXunit/DemoLibrary.Tests/ExamplesTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+
+namespace DemoLibrary.Tests
+{
+    public class ExamplesTests
+    {
+        [Fact]
+        public void ExampleLoadTextFile_ValidNameShouldWork()
+        {
+            string actual = Examples.ExampleLoadTextFile("This is a valid file name.");
+
+            Assert.True(actual.Length > 0);
+        }
+
+        [Fact]
+        public void ExampleLoadTextFile_InvalidNameShouldFail()
+        {
+            Assert.Throws<ArgumentException>("file", () => Examples.ExampleLoadTextFile(""));
+        }
+    }
+}
